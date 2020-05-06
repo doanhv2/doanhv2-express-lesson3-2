dc4ea921776beca19fa98e2bed7384c9cef4db06
Glitch (hello-express) <none>
2020-05-06 23:26:10 +0000

    🍛⏲ Checkpoint ./server.js:15887900/50

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 
